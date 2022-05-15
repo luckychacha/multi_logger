f2992cdba01a66c2ddb8843dda175d218290298b
luckychacha <358140284@qq.com>
2022-05-15 20:57:31 +0800

    Initial commit.

---
+++ src/multi_logger_assets/src/index.js
@@ -0,0 +1,19 @@
+import { multi_logger } from "../../declarations/multi_logger";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await multi_logger.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
